ff91a09c97099f7c01bd6a1b2e0679ad23dfd87b
sagar-narola <shm.narola@gmail.com>
2018-03-30 17:00:19 +0530

    fix field name

--- db/migrate/20180330091957_create_user_service_wallets.rb
+++ db/migrate/20180330091957_create_user_service_wallets.rb
@@ -1,7 +1,7 @@
 class CreateUserServiceWallets < ActiveRecord::Migration[5.1]
   def change
     create_table :user_service_wallets do |t|
-      t.references :user_services, foreign_key: true
+      t.references :user_service, foreign_key: true
       t.integer :wallet_amount
       t.timestamps
     end
