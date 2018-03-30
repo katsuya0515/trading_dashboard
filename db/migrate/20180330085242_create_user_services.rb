4f9b37fdc7ba635f5bb682ad193ea4da847c6725
sagar-narola <shm.narola@gmail.com>
2018-03-30 14:36:31 +0530

    add files for user services

---
+++ db/migrate/20180330085242_create_user_services.rb
@@ -0,0 +1,9 @@
+class CreateUserServices < ActiveRecord::Migration[5.1]
+  def change
+    create_table :user_services do |t|
+      t.references :user , foreign_key: true
+      t.references :service , foreign_key: true
+      t.timestamps
+    end
+  end
+end
