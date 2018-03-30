db027604b066f50471014e74e9db3fd0b1c188d6
sagar-narola <shm.narola@gmail.com>
2018-03-30 12:54:02 +0530

    modified relationship for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
   has_many :user_accounts, dependent: :destroy
   has_many :user_tradings, dependent: :destroy
-  
+
 end
